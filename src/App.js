ab1e80dd026feb1ecadbe7a30c5fffa8145a5ed3
Sanjay Purswani <sanjsanj@hotmail.com>
2019-03-06 19:59:02 +0000

    Trail in data's opacity and size

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import Parallax from "./Parallax";
+import {
+  useTransition,
+  useSpring,
+  useChain,
+  config,
+  useTrail,
+  animated,
+} from "react-spring";
 
 function App() {
   const [data, setData] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
 
-  async function fetchData() {
+  const fetchData = async () => {
     setLoading(true);
 
     const fetchResp = await fetch(`https://swapi.co/api/people/`);
@@ -13,25 +21,48 @@ function App() {
 
     setData(fetchJson.results);
     setLoading(false);
-  }
+  };
 
   React.useEffect(() => {
     fetchData();
   }, []);
+
+  // const transRef = React.useRef();
+  // const transitions = useTransition(data, person => person.name, {
+  //   ref: transRef,
+  //   unique: true,
+  //   trail: 4000,
+  //   from: { opacity: 0, fontSize: "10px" },
+  //   enter: { opacity: 1, fontSize: "24px" },
+  //   leave: { opacity: 0, fontSize: "10px" },
+  // });
+
+  const config = { mass: 5, tension: 2000, friction: 200 };
+  const trail = useTrail(data.length, {
+    config,
+    from: { opacity: 0, fontSize: "10px" },
+    to: { opacity: 1, fontSize: "60px" },
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Title</h1>
       </header>
+
       {/* <Parallax /> */}
+
+      <button onClick={() => fetchData()}>Load</button>
+
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        data.map(person => {
-          console.log(person);
-          return <p key={person.name}>{person.name}</p>;
-        })
+        // data.length &&
+        trail.map((props, index) => (
+          <animated.div key={index} style={props}>
+            {data[index].name}
+          </animated.div>
+        ))
       )}
     </div>
   );
